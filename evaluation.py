2f19a7f1c2d214da2503a0c36587ed8cec37244e
farzadashfak <116588610+farzadashfak@users.noreply.github.com>
2023-04-07 13:12:14 -0400

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -10,5 +10,5 @@ def mean_absolute_error(truth: pd.core.series.Series, prediction: np.ndarray):
     assert truth.size == prediction.shape[0], "Truth and Prediction must have same number of entries" 
     truth = truth.to_numpy()
     prediction = [_[0] for _ in prediction]
-    return np.mean(abs(truth - prediction));
+    return np.mean(abs(truth - prediction)); 
 
